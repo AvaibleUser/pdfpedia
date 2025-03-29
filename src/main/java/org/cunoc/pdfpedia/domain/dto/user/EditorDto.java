147bbbb33e25842f12de31a751769be6afd5408b
alodev <brayan.quialo@gmail.com>
2025-03-29 09:16:50 -0600

    feat(profile): Add update and read for profile

--- src/main/java/org/cunoc/pdfpedia/domain/dto/user/EditorDto.java
+++ src/main/java/org/cunoc/pdfpedia/domain/dto/user/EditorDto.java
@@ -6,12 +6,14 @@ import org.cunoc.pdfpedia.domain.entity.user.UserEntity;
 public record EditorDto(
         Long id,
         String username,
+        String email,
         EditorProfileDto profile
 ) {
     public static EditorDto fromEntity(UserEntity editor) {
         return new EditorDto(
                 editor.getId(),
                 editor.getUsername(),
+                editor.getEmail(),
                 EditorProfileDto.fromEntity(editor.getProfile())
         );
     }
